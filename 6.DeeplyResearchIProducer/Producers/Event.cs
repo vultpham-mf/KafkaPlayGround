bcca868fa4ce874de12e5516e2d24f98766585d3
vultpham-mf <vultpham.mf@gmail.com>
2025-12-25 21:30:07 +0700

    6 | Replace "CompareProduceAndProduceAsync" project with "DeeplyResearchIProducer" project: includes Kafka producer setup, custom serializer, new API endpoints, and solution integration.

---
+++ 6.DeeplyResearchIProducer/Producers/Event.cs
@@ -0,0 +1,6 @@
+namespace _6.DeeplyResearchIProducer.Producers;
+
+public record Event
+{
+    public required string Message { get; init; }
+}
---
+++ 6.DeeplyResearchIProducer/Producers/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+
+namespace _6.DeeplyResearchIProducer.Producers;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddProducer(this IServiceCollection services)
+    {
+        services
+            .AddOptionsWithValidateOnStart<ProducerSettings>()
+            .BindConfiguration(nameof(ProducerSettings))
+            .ValidateDataAnnotations();
+        
+        services.AddSingleton<IProducer<string, Event>>(serviceProvider =>
+        {
+            var settings = serviceProvider.GetRequiredService<IOptions<ProducerSettings>>().Value;
+
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = settings.BootstrapServers,
+                SaslUsername = settings.SaslUsername,
+                SaslPassword = settings.SaslPassword,
+                
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslMechanism = SaslMechanism.Plain,
+                Acks = Acks.All,
+            };
+            
+            return new ProducerBuilder<string, Event>(producerConfig)
+                .SetKeySerializer(Serializers.Utf8)
+                .SetValueSerializer(new CustomJsonSerializer<Event>())
+                .Build();
+        });
+        
+        return services;
+    }
+}
